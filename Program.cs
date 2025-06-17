0e4cd9ba78f9f1a2140fc7b36db7d694ddbf570e
10MiA01 <miklashevichanastasiyachelovek@gmail.com>
2025-06-17 22:39:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JFjelelery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bot started working");
+            Console.ReadLine();
+            Console.WriteLine("Bot ended working");
+        }
+    }
+}
